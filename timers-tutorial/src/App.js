02f0be4f9c9d56b575f1b469e38f257d7b01f693
mcdott <111994803+mcdott@users.noreply.github.com>
2023-12-23 17:04:29 -0500

    Adds new timer input

--- timers-tutorial/src/App.js
+++ timers-tutorial/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NewTimer from "./components/NewTimer";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NewTimer />
+    </div>
+  );
 }
 
 export default App;
