4485b45a1b860fde9fd1c828b2133f3ff74a8c68
mcdott <111994803+mcdott@users.noreply.github.com>
2023-12-23 19:38:57 -0500

    Adds list of timers

--- timers-tutorial/src/App.js
+++ timers-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import ListTimers from "./components/ListTimers";
 import NewTimer from "./components/NewTimer";
 
 function App() {
   return (
     <div className='App'>
       <NewTimer />
+      <ListTimers />
     </div>
   );
 }
