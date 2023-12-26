5ba4cb981a492359dd83d45089a45ec76bdbb597
mcdott <111994803+mcdott@users.noreply.github.com>
2023-12-26 09:16:06 -0500

    Styles button colors depending on state as per stretch goal

--- timers-tutorial/src/components/TimerView.js
+++ timers-tutorial/src/components/TimerView.js
@@ -20,11 +20,16 @@ function formatTime(time) {
 export default function TimerView({ index, name, time, isRunning }) {
   const dispatch = useDispatch();
 
+  const buttonClass = isRunning ? "button-stop" : "button-start";
+
   return (
     <div className='TimerView'>
       <h2>{name}</h2>
       <h1>{formatTime(time)}</h1>
-      <button onClick={() => dispatch(toggleTimer(index))}>
+      <button
+        className={buttonClass}
+        onClick={() => dispatch(toggleTimer(index))}
+      >
         {isRunning ? "Stop" : "Start"}
       </button>
     </div>
